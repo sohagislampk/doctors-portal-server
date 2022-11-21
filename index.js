3e200a51b215b2ae9cd084c9c54a3f3b8faf1e2d
sohagislampk <sohagislampk@gmail.com>
2022-11-21 17:44:00 +0600

    limit appointment by date

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ async function run() {
         })
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+
+            const alreadyBooked = await bookingCollections.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledged: false, message })
+            }
 
             const result = await bookingCollections.insertOne(booking);
             res.send(result);
