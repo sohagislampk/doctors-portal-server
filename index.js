0ff3c62ec16a81a9c87d8725bfd5d5623ab60ad7
sohagislampk <sohagislampk@gmail.com>
2022-11-21 18:08:53 +0600

    Load bookings by email

--- index.js
+++ index.js
@@ -34,7 +34,15 @@ async function run() {
             })
 
             res.send(options);
-        })
+        });
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+
+            const query = { email: email };
+            const bookings = await bookingCollections.find(query).toArray();
+            res.send(bookings);
+        });
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const query = {
